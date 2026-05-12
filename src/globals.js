f6da012a9e0adb6bb0fc6f0edcb32d0fadaaf587
Léo Debonnet <debonnetleo4@gmail.com>
2026-05-12 18:29:27 +0200

    setting up global vars

--- src/globals.js
+++ src/globals.js
@@ -8,18 +8,38 @@ export const globals = {
     },
     server: {
         channel: {
-            fill: ``
+            flow: `1478130659326300355`,
+            rules: `1478130894563967058`,
+            missionBoard: `1478133200109965497`,
+            annoucements: `1481027774465708216`,
+            general: `1478130580712460480`,
         },
         role: {
-            fill: ``
+            mod: `1483148976101396480`,
+            kage: `1478134870247997723`,
+            sub: `1478134948861841498`,
+            member: `1495848176286957599`,
+            lone: `1503787085172052088`,
+            pingServer: `1479553348373057666`,
+            pingArena: `1479552402863558727`,
+            pingAuction: `1478135570696769638`,
+            pingNinjaWar: `1478135456393597008`,
         },
         member: {
             fill: ``
         }
     },
     embed: {
-        colorMain: `#ccfffc`, // Light blue
-        colorSecond: `#bbffb3`, // Light green
-        colorError: `#ff0000` // Red
+        red: 0xed4a4a,
+        orange: 0xde8240,
+        yellow: 0xd1cf3d,
+        green: 0x73d138,
+        cyan: 0x3bccc5,
+        blue: 0x3985e3,
+        purple: 0x934aed,
+        pink: 0xe343e6,
+        white: 0xe3e3e3,
+        grey: 0x8f8f8f,
+        black: 0x050505
     }
 }
